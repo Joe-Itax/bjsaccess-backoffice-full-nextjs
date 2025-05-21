619cb51e61745854db8285d48e0306948451731e
Joseph Itakala <josephitakala18@gmail.com>
2025-05-21 12:31:56 +0200

    .

--- src/lib/middlewares/require-role.ts
+++ src/lib/middlewares/require-role.ts
@@ -2,18 +2,6 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-// export async function requireRole(req: Request, role: "ADMIN" | "AUTHOR") {
-//   const session = await auth.api.getSession({ headers: await headers() });
-
-//   if (!session || session.user.role !== role) {
-//     return new Response("Vous n'êtes pas autorisé à éffectué cette action.", {
-//       status: 403,
-//     });
-//   }
-
-//   return null;
-// }
-
 export async function requireRole(requiredRoles: string | string[]) {
   if (!Array.isArray(requiredRoles)) {
     requiredRoles = [requiredRoles];
